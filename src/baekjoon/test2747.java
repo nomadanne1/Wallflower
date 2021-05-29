6402d03f22d6eea3c57e220d2799c2c5aea87bc1
CookieYi <nomadanne1@naver.com>
2021-05-29 22:22:35 +0900

    피보나치 수2

---
+++ src/baekjoon/test2747.java
@@ -0,0 +1,51 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class test2747 {
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int input = sc.nextInt();
+		
+		// 0 1 1 2 3 5 8 ...
+		int fibo1 = 0;
+		int fibo2 = 1;
+		int fibo3 = 1;
+	
+		for (int i=2; i<=input; i++) {
+			fibo3 = fibo1 + fibo2; 
+			fibo1 = fibo2;
+			fibo2 = fibo3;
+		}
+		if(input == 0) {
+			System.out.println(fibo1);
+		}else if(input == 1){
+			System.out.println(fibo2);
+		}else {
+			System.out.println(fibo3);			
+		}
+	
+		
+		
+	}
+}
+//  재귀함수 사용 -> 시간초과
+//	public static void main (String[] args) {
+//		
+//		Scanner sc = new Scanner(System.in);
+//		int input = sc.nextInt();
+//		
+//		System.out.println(Fibonacci(input));
+//	}
+//	
+//	// 0 1 1 2 3 5 8 ...
+//	public static int Fibonacci (int input) {
+//		if (input == 0) { return 0;	}
+//		if (input == 1) { return 1;	}
+//		
+//		int result = Fibonacci(input-1) + Fibonacci(input-2);
+//		
+//		return result;
+//	}
+

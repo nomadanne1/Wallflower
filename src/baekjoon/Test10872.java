3480fd2d6f03a6bb87cb913933b798dbd6928ddc
CookieYi <nomadanne1@naver.com>
2021-06-03 04:16:20 +0900

    팩토리얼 >> 재귀함수로 문제풀이

--- src/baekjoon/Test10872.java
+++ src/baekjoon/Test10872.java
@@ -5,14 +5,27 @@ import java.util.Scanner;
 // ÆÑÅä¸®¾ó
 public class Test10872 {
 	public static void main(String[] args) {
+//		Scanner sc = new Scanner(System.in);
+//		int N = sc.nextInt();
+//		
+//		int fac = 1;
+//		for (int i=1; i<=N; i++) {
+//			fac *= i;
+//		}
+//
+//		System.out.println(fac);	
+		
+		// Àç±ÍÇÔ¼ö
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		
-		int fac = 1;
-		for (int i=1; i<=N; i++) {
-			fac *= i;
+		System.out.println(fac(N));
+	}
+	
+	public static int fac(int N) {
+		if (N <= 1) {
+			return 1;
 		}
-
-		System.out.println(fac);	
+		return fac(N-1)*N;
+		
 	}
 }
